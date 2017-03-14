cc70567f2013851bc6f73882f71cc2da0b286a51
MisuBeImp <bsse0516@iit.du.ac.bd>
2017-03-15 00:40:33 +0600

    added java-symbol-solver

--- src/iit/du/ac/bd/misubeimp/analyzer/MethodCallsExample.java
+++ src/iit/du/ac/bd/misubeimp/analyzer/MethodCallsExample.java
@@ -6,13 +6,25 @@ import java.io.IOException;
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.model.typesystem.Type;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import com.google.common.base.Strings;
 
 import iit.du.ac.bd.misubeimp.support.DirectoryExplorer;
 
 public class MethodCallsExample {
-
+	
     public static void listMethodCalls(File projectDir) {
+    	
+    	CombinedTypeSolver combinedTypeSolver= new CombinedTypeSolver();
+    	combinedTypeSolver.add(new ReflectionTypeSolver());
+        combinedTypeSolver.add(new JavaParserTypeSolver(new File("D:\\My All Lab Work\\JAVA LAB\\Design Pattern\\Design Pattern Template Method\\src")));
+       
+    	
         new DirectoryExplorer((level, path, file) -> path.endsWith(".java"), (level, path, file) -> {
             System.out.println(path);
             System.out.println(Strings.repeat("=", path.length()));
@@ -22,6 +34,7 @@ public class MethodCallsExample {
                     public void visit(MethodCallExpr n, Object arg) {
                         super.visit(n, arg);
                         System.out.println(" [L " + n.getRange() + "] " + n);
+                        
                     }
                 }.visit(JavaParser.parse(file), null);
                 System.out.println(); // empty line
@@ -32,7 +45,7 @@ public class MethodCallsExample {
     }
 
     public static void main(String[] args) {
-        File projectDir = new File("C:\\Users\\MisuBeImp\\Downloads\\UltimateCalculator-master\\UltimateCalculator-master");
+        File projectDir = new File("D:\\My All Lab Work\\JAVA LAB\\Design Pattern\\Design Pattern Template Method");
         listMethodCalls(projectDir);
         int m=0;
     }
